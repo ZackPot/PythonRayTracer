1da4923674e4c5dc6cf2296000fb05f36b384c38
Zachary Potishko <130048004+ZackPot@users.noreply.github.com>
2026-04-15 09:00:18 +0800

    Added a shape and camera class and added base code for creating a camera and a triangular pyramid.

--- raytracer.py
+++ raytracer.py
@@ -1,8 +1,47 @@
 import numpy as np
 
+class Camera:
+    def __init__(self, origin: np.typing.NDArray):
+        self.origin = origin
+        self.camera_plane = None
+
+    def update_camera_plane(self, target_pos, size=2.0, res=10):
+        target_pos = np.array([float(point) for point in target_pos])
+        forward = target_pos - self.origin
+        forward /= np.linalg.norm(forward)
+
+        world_up = np.array([0, 1, 0])
+        right = np.cross(world_up, forward)
+        right /= np.linalg.norm(right)
+        up = np.cross(forward, right)
+
+        plane_center = self.origin + (forward * 2)
+
+        grid_points = []
+        ticks = np.linspace(-size / 2, size / 2, res)
+        for i in ticks:
+            for j in ticks:
+                point = plane_center + (i * right) + (j * up)
+                grid_points.append(point)
+
+        half = size / 2
+        corners = np.array([
+            plane_center - half * right - half * up,
+            plane_center - half * right + half * up,
+            plane_center + half * right + half * up,
+            plane_center + half * right - half * up
+        ])
+
+        self.camera_plane = np.vstack((grid_points, corners))
+        print(self.camera_plane)
+
 class Face:
     def __init__(self, vertices: np.typing.NDArray):
         self.vertices = vertices
+
+class Shape:
+    def __init__(self, faces: np.typing.NDArray):
+        self.faces = faces
 
 class Ray:
     def __init__(self, brightness: int, origin: np.typing.NDArray, direction: np.typing.NDArray):
@@ -10,26 +49,27 @@ class Ray:
         self.origin = origin
         self.direction = direction
 
-    def bounce(self, collision_point: np.typing.NDArray, light: np.typing.NDArray, faces: np.typing.NDArray):
-        self.direction = collision_point - light
-
-        for face in faces:
+    @staticmethod
+    def detect_collision(shape_faces, direction, origin):
+        for face in shape_faces:
             edge1 = face.vertices[1] - face.vertices[0]
             edge2 = face.vertices[2] - face.vertices[0]
             normal = np.linalg.cross(edge1, edge2)
             weight1 = 2
             weight2 = 5
-            A = (face.vertices[0] + weight1 * (face.vertices[1] - face.vertices[0]) + weight2
+            a = (face.vertices[0] + weight1 * (face.vertices[1] - face.vertices[0]) + weight2
                  * (face.vertices[2] - face.vertices[0]))
 
-            if np.round(np.dot(normal, self.direction), 2) == 0:
-                pass
+            if np.round(np.dot(normal, direction), 2) == 0:
+                dist = np.sqrt(np.sum(direction ** 2))
+                brightness = 1 / dist ** 2
+                return brightness
             else:
-                t = np.dot((A - self.origin), normal) / np.dot(normal, self.direction)
+                t = np.dot((a - origin), normal) / np.dot(normal, direction)
                 if t < 0:
                     pass
                 else:
-                    intersection = self.origin + t * self.direction
+                    intersection = origin + t * direction
                     v0 = face.vertices[1] - face.vertices[0]
                     v1 = face.vertices[2] - face.vertices[0]
                     v2 = intersection - face.vertices[0]
@@ -40,12 +80,32 @@ class Ray:
                     d20 = v2 @ v0
                     d21 = v2 @ v1
 
-                    denom = d00 * d11 - d01 * d01
-                    u = (d11 * d20 - d01 * d21) / denom
-                    v = (d00 * d21 - d01 * d20) / denom
+                    denominator = d00 * d11 - d01 * d01
+                    u = (d11 * d20 - d01 * d21) / denominator
+                    v = (d00 * d21 - d01 * d20) / denominator
 
                     if u >= 0 and v >= 0 and (u + v) <= 1:
-                        self.brightness = 0
+                        return 0
                     else:
-                        dist = np.sqrt(np.sum(self.direction ** 2))
-                        self.brightness = 1 / dist ** 2
+                        dist = np.sqrt(np.sum(direction ** 2))
+                        brightness = 1 / dist ** 2
+                        return brightness
+        return 0
+
+    def bounce(self, collision_point: np.typing.NDArray, light: np.typing.NDArray, faces: np.typing.NDArray):
+        self.direction = collision_point - light
+        self.brightness = self.detect_collision(faces, self.direction, self.origin)
+
+camera = Camera(np.array([0, 0, 10]))
+camera.update_camera_plane(np.array([0, 0, 0]))
+
+pyramid_faces = []
+
+face1 = Face(np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]]))
+face2 = Face(np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0]]))
+face3 = Face(np.array([[0, 0, 0], [0, 0, 1], [1, 0, 0]]))
+face4 = Face(np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]]))
+pyramid_faces.append(np.array([face1, face2, face3, face4]))
+pyramid_faces = np.array(pyramid_faces)
+
+pyramid = Shape(pyramid_faces)
